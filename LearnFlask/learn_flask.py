310081c16dddfaa038814cb74d9807ccc2ee5a78
James <james315@icloud.com>
2021-01-17 20:05:41 -0500

    Final version (completes course)

--- LearnFlask/learn_flask.py
+++ LearnFlask/learn_flask.py
@@ -1,18 +1,14 @@
 
-from flask import Flask, render_template, abort, jsonify
+from flask import Flask, render_template, abort, request, redirect, url_for
 import model
 
 
 app = Flask(__name__)
 
-view_count = 0
-
 
 @app.route("/")
 def welcome_view():
-    global view_count
-    view_count += 2
-    return render_template("welcome.html", db=model.db, views=view_count)
+    return render_template("welcome.html", db=model.db)
 
 
 @app.route("/salary/<int:index>")
@@ -23,6 +19,32 @@ def salary_view(index):
                                salary_record=salary_record,
                                index=index,
                                max_index=len(model.db)-1)
+    except IndexError:
+        abort(404)
+
+
+@app.route("/add_salary", methods=["GET", "POST"])
+def add_salary():
+    if request.method == "POST":
+        salary_record = {"name": request.form['name'],
+                         "salary": request.form['salary']}
+        model.db.append(salary_record)
+        model.save_db()
+        return redirect(url_for('salary_view', index=len(model.db)-1))
+    else:
+        return render_template("add_salary.html")
+
+
+@app.route("/delete_salary/<int:index>", methods=["GET", "POST"])
+def delete_salary(index):
+    try:
+        if request.method == "POST":
+            del model.db[index]
+            model.save_db()
+            return redirect(url_for('welcome_view'))
+        else:
+            return render_template("delete_salary.html",
+                                   salary_record=model.db[index])
     except IndexError:
         abort(404)
 
